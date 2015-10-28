76b7d23a554baf3d16ff2a4b5a47fcb3680b2ff6
dantesc3 <kid_jx@hotmail.com>
2015-10-27 22:46:37 -0600

    daniel

---
+++ mayor_y_menor.c
@@ -0,0 +1,155 @@
+#include<stdio.h>
+int a,b,c,d,e,f,g,h,i,j;
+int main()
+{
+	printf("ingrese el 1;numero\n");
+	scanf("%d",&a);
+	printf("ingrese el 2;numero\n");
+	scanf("%d",&b);
+	printf("ingrese el 3;numero\n");
+	scanf("%d",&c);
+	printf("ingrese el 4;numero\n");
+	scanf("%d",&d);
+	printf("ingrese el 5;numero\n");
+	scanf("%d",&e);
+	printf("ingrese el 6;numero\n");
+	scanf("%d",&f);
+	printf("ingrese el 7;numero\n");
+	scanf("%d",&g);
+	printf("ingrese el 8;numero\n");
+	scanf("%d",&h);
+	printf("ingrese el 9;numero\n");
+	scanf("%d",&i);
+	printf("ingrese el 10;numero\n");
+	scanf("%d",&j);
+	if
+		(a>b && a>c && a>d && a>e && a>f && a>g && a>h && a>i && a>j)
+		{
+				printf ("numero mayor es:%d\n",a);
+				return 0;
+		}
+	{
+	if
+		(b>a && b>c && b>d && b>e && b>f && b>g && b>h && b>i && b>j){
+		
+				printf ("numero mayor es:%d\n",b);
+			
+		}
+		if
+		(c>a && c>b && c>d && c>e && c>f && c>g && c>h && c>i && c>j){
+		
+				printf ("numero mayor es:%d\n",c);
+				
+		}
+		if
+		(d>a && d>b && d>c && d>e && d>f && d>g && d>h && d>i && d>j){
+		
+				printf ("numero mayor es:%d\n",d);
+				
+		}
+		if
+		(e>a && e>b && e>c && e>d && e>f && e>g && e>h && e>i && e>j){
+		
+				printf ("numero mayor es:%d\n",e);
+			
+		}
+		if
+		(f>a && f>b && f>c && f>d && f>e && f>g && f>h && f>i && f>j){
+		
+				printf ("numero mayor es:%d\n",f);
+				
+		}
+		if
+		(g>a && g>b && g>c && g>d && g>e && g>f && g>h && g>i && g>j){
+		
+				printf ("numero mayor es:%d\n",g);
+				
+		}
+		if
+		(h>a && h>b && h>c && h>d && h>e && h>f && h>g && h>i && h>j){
+		
+				printf ("numero mayor es:%d\n",h);
+				
+				
+		}
+		if
+		(i>a && i>b && i>c && i>d && i>e && i>f && i>g && i>h && i>j){
+		
+				printf ("numero mayor es:%d\n",i);
+			
+		}
+		if
+		(j>a && j>b && j>c && j>d && j>e && j>f && j>g && j>h && j>i){
+		
+				printf ("numero mayor es:%d\n",j);
+		
+		}
+	}
+	if
+		(a<b && a<c && a<d && a<e && a<f && a<g && a<h && a<i && a<j)
+		{
+				printf ("numero menor es:%d\n",a);
+				return 0;
+		}
+	{
+	if
+		(b<a && b<c && b<d && b<e && b<f && b<g && b<h && b<i && b<j){
+		
+				printf ("numero menor es:%d\n",b);
+			
+		}
+		if
+		(c<a && c<b && c<d && c<e && c<f && c<g && c<h && c<i && c<j){
+		
+				printf ("numero menor es:%d\n",c);
+				
+		}
+		if
+		(d<a && d<b && d<c && d<e && d<f && d<g && d<h && d<i && d<j){
+		
+				printf ("numero menor es:%d\n",d);
+				
+		}
+		if
+		(e<a && e<b && e<c && e<d && e<f && e<g && e<h && e<i && e<j){
+		
+				printf ("numero menor es:%d\n",e);
+			
+		}
+		if
+		(f<a && f<b && f<c && f<d && f<e && f<g && f<h && f<i && f<j){
+		
+				printf ("numero menor es:%d\n",f);
+				
+		}
+		if
+		(g<a && g<b && g<c && g<d && g<e && g<f && g<h && g<i && g<j){
+		
+				printf ("numero menor es:%d\n",g);
+				
+		}
+		if
+		(h<a && h<b && h<c && h<d && h<e && h<f && h<g && h<i && h<j){
+		
+				printf ("numero menor es:%d\n",h);
+				
+				
+		}
+		if
+		(i<a && i<b && i<c && i<d && i<e && i<f && i<g && i<h && i<j){
+		
+				printf ("numero menor es:%d\n",i);
+			
+		}
+		if
+		(j<a && j<b && j<c && j<d && j<e && j<f && j<g && j<h && j<i){
+		
+				printf ("numero menor es:%d\n",j);
+		
+		}
+	}
+	
+	
+	
+}
+
