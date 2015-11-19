ab0dcd213b8fa2dbd61c3e0b28948547bbccdf2b
dantesc3 <kid_jx@hotmail.com>
2015-11-18 18:23:58 -0600

    daniel

---
+++ multiplos2.c
@@ -0,0 +1,12 @@
+# include <stdio.h> 
+int b= 1, c= 0, n1;
+int main () 
+{  
+printf("dame un numero\n");
+scanf("%d",&n1);
+while (b <= 10) 
+{ c= b * n1; 
+printf ("\n %del multiplo es:%d", b, c); 
+b++; 
+} 
+} 
